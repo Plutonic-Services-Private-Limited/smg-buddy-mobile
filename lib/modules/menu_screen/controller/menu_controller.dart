35adfeff4681a3c1e1b8e2063ccb1cd22b85ec0d
ayush-kashyap <ayushkashyap0507@gmail.com>
2024-11-22 20:09:05 +0530

    changed menu pic

--- lib/modules/menu_screen/controller/menu_controller.dart
+++ lib/modules/menu_screen/controller/menu_controller.dart
@@ -1,5 +1,5 @@
 import 'package:get/get.dart';
 import 'package:smg_buddy_app/constants/assets.dart';
  class CustomMenuController extends GetxController{
-  var img="https://images.pexels.com/photos/769772/pexels-photo-769772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+  var img="https://images.pexels.com/photos/29490751/pexels-photo-29490751/free-photo-of-sunny-beach-day-in-gaspe-quebec-canada.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
  }
